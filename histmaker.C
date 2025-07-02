e1e93da6908fb08ce8a5a07043049df4bc8b0fea
Dorottya judit Cynolter <dcynolte@srv-b1b08-18-01.cern.ch>
2025-07-03 00:45:05 +0200

    Adding ratio plots

--- histmaker.C
+++ histmaker.C
@@ -133,17 +133,31 @@ void histmaker(){
     // use HFONLINE and HFOFFLINE functions for a tcut
     // use TRKPT for track pt cuts
 
-    // EXAMPLE EVENT SELECTIONs
-    TCut ESel_Example1 = VZcut && PVcut && CCcut && Nvtxcut && HFONLINE(14, "&&");
-    TCut ESel_Example2 = VZcut && PVcut && CCcut && Nvtxcut && HFOFFLINE(10, "||") && HFONLINE(14, "||");
+    //EVENT SELECTIONs
 
     TCut eventsel = VZcut && PVcut && CCcut && Nvtxcut;
-    TCut online14AND = HFONLINE(14, "&&");
-    TCut online14OR = HFONLINE(14, "||");
-    TCut online16OR = HFONLINE(16, "&&");
-    TCut online16AND = HFONLINE(16, "||");
-    TCut event_online14AND = eventsel && online14AND;
+    TCut eventsel_pt2 = eventsel && TRKPTCUT(2.0);
+    TCut eventsel_pt3 = eventsel && TRKPTCUT(3.0);
+    TCut eventsel_pt4 = eventsel && TRKPTCUT(4.0);
+    TCut event_online14AND = eventsel && HFONLINE(14, "&&");
     TCut event_online14AND_pt2 = event_online14AND && TRKPTCUT(2.0);
+    TCut event_online14AND_pt3 = event_online14AND && TRKPTCUT(3.0);
+    TCut event_online14AND_pt4 = event_online14AND && TRKPTCUT(4.0);
+
+    TCut event_online14OR = eventsel && HFONLINE(14, "||");
+    TCut event_online14OR_pt2 = event_online14OR && TRKPTCUT(2.0);
+    TCut event_online14OR_pt3 = event_online14OR && TRKPTCUT(3.0);
+    TCut event_online14OR_pt4 = event_online14OR && TRKPTCUT(4.0);
+
+    TCut event_online16AND = eventsel && HFONLINE(16, "&&");
+    TCut event_online16AND_pt2 = event_online16AND && TRKPTCUT(2.0);
+    TCut event_online16AND_pt3 = event_online16AND && TRKPTCUT(3.0);
+    TCut event_online16AND_pt4 = event_online16AND && TRKPTCUT(4.0);
+
+    TCut event_online16OR = eventsel && HFONLINE(16, "||");
+    TCut event_online16OR_pt2 = event_online16OR && TRKPTCUT(2.0);
+    TCut event_online16OR_pt3 = event_online16OR && TRKPTCUT(3.0);
+    TCut event_online16OR_pt4 = event_online16OR && TRKPTCUT(4.0);
 
     /////////////////////////////
     /////////////////////////////
@@ -154,13 +168,80 @@ void histmaker(){
     /////////////////////////////
 
     TH1D* minHFEMaxEvent = new TH1D("minHFEMaxeventsel", "HFEMax min with event selection", 201, -0.5, 200.5);
+    TH1D *minHFEMaxEventpt2 = new TH1D("minHFEMaxeventsel_pt2", "HFEMax min with event selection and pt > 2", 201, -0.5, 200.5);
+    TH1D *minHFEMaxEventpt3 = new TH1D("minHFEMaxeventsel_pt3", "HFEMax min with event selection and pt > 3", 201, -0.5, 200.5);
+    TH1D *minHFEMaxEventpt4 = new TH1D("minHFEMaxeventsel_pt4", "HFEMax min with event selection and pt > 4", 201, -0.5, 200.5);
     TH1D* minHFEMaxEventOnline14AND = new TH1D("minHFEMaxevent_online14AND", "HFEMax min with event and online 14 AND selection", 201, -0.5, 200.5);
+    TH1D* minHFEMaxEventOnline14ANDpt2 = new TH1D("minHFEMaxevent_online14AND_pt2", "HFEMax min with event and online 14 AND pt > 2 selection", 201, -0.5, 200.5);
+    TH1D* minHFEMaxEventOnline14ANDpt3 = new TH1D("minHFEMaxevent_online14AND_pt3", "HFEMax min with event and online 14 AND pt > 3 selection", 201, -0.5, 200.5);
+    TH1D* minHFEMaxEventOnline14ANDpt4 = new TH1D("minHFEMaxevent_online14AND_pt4", "HFEMax min with event and online 14 AND pt > 4 selection", 201, -0.5, 200.5);
 
     HFEMax_Minimum(T, eventsel, minHFEMaxEvent);
+    HFEMax_Minimum(T, eventsel_pt2, minHFEMaxEventpt2);
+    HFEMax_Minimum(T, eventsel_pt3, minHFEMaxEventpt3);
+    HFEMax_Minimum(T, eventsel_pt4, minHFEMaxEventpt4);
     HFEMax_Minimum(T, event_online14AND, minHFEMaxEventOnline14AND);
+    HFEMax_Minimum(T, event_online14AND_pt2, minHFEMaxEventOnline14ANDpt2);
+    HFEMax_Minimum(T, event_online14AND_pt3, minHFEMaxEventOnline14ANDpt3);
+    HFEMax_Minimum(T, event_online14AND_pt4, minHFEMaxEventOnline14ANDpt4);
 
     TH1D* ratio14AND = Divide(minHFEMaxEventOnline14AND, minHFEMaxEvent, "ratio14AND");
-    
+    TH1D* ratio14ANDpt2 = Divide(minHFEMaxEventOnline14ANDpt2, minHFEMaxEventpt2, "ratio14ANDpt2");
+    TH1D* ratio14ANDpt3 = Divide(minHFEMaxEventOnline14ANDpt3, minHFEMaxEventpt3, "ratio14ANDpt3");
+    TH1D* ratio14ANDpt4 = Divide(minHFEMaxEventOnline14ANDpt4, minHFEMaxEventpt4, "ratio14ANDpt4");
+
+    TH1D *maxHFEMaxEvent = new TH1D("maxHFEMaxeventsel", "HFEMax max with event selection", 201, -0.5, 200.5);
+    TH1D *maxHFEMaxEventpt2 = new TH1D("maxHFEMaxeventsel_pt2", "HFEMax max with event selection and pt > 2", 201, -0.5, 200.5);
+    TH1D *maxHFEMaxEventpt3 = new TH1D("maxHFEMaxeventsel_pt3", "HFEMax max with event selection and pt > 3", 201, -0.5, 200.5);
+    TH1D *maxHFEMaxEventpt4 = new TH1D("maxHFEMaxeventsel_pt4", "HFEMax max with event selection and pt > 4", 201, -0.5, 200.5);
+    TH1D *maxHFEMaxEventOnline14OR= new TH1D("maxHFEMaxevent_online14OR", "HFEMax max with event and online 14 OR selection", 201, -0.5, 200.5);
+    TH1D *maxHFEMaxEventOnline14ORpt2 = new TH1D("maxHFEMaxevent_online14ORpt2", "HFEMax max with event and online 14 OR  and pt > 2 selection", 201, -0.5, 200.5);
+    TH1D *maxHFEMaxEventOnline14ORpt3 = new TH1D("maxHFEMaxevent_online14ORpt3", "HFEMax max with event and online 14 OR  and pt > 3 selection", 201, -0.5, 200.5);
+    TH1D *maxHFEMaxEventOnline14ORpt4 = new TH1D("maxHFEMaxevent_online14ORpt4", "HFEMax max with event and online 14 OR  and pt > 4 selection", 201, -0.5, 200.5); 
+
+    HFEMax_Maximum(T, eventsel, maxHFEMaxEvent);
+    HFEMax_Maximum(T, eventsel_pt2, maxHFEMaxEventpt2);
+    HFEMax_Maximum(T, eventsel_pt3, maxHFEMaxEventpt3);
+    HFEMax_Maximum(T, eventsel_pt4, maxHFEMaxEventpt4);
+    HFEMax_Maximum(T, event_online14OR, maxHFEMaxEventOnline14OR);
+    HFEMax_Maximum(T, event_online14OR_pt2, maxHFEMaxEventOnline14ORpt2);
+    HFEMax_Maximum(T, event_online14OR_pt3, maxHFEMaxEventOnline14ORpt3);
+    HFEMax_Maximum(T, event_online14OR_pt4, maxHFEMaxEventOnline14ORpt4);
+
+    TH1D* ratio14OR = Divide(maxHFEMaxEventOnline14OR, maxHFEMaxEvent, "ratio14OR");
+    TH1D* ratio14ORpt2 = Divide(maxHFEMaxEventOnline14ORpt2, maxHFEMaxEventpt2, "ratio14ORpt2");
+    TH1D* ratio14ORpt3 = Divide(maxHFEMaxEventOnline14ORpt3, maxHFEMaxEventpt3, "ratio14ORpt3");
+    TH1D* ratio14ORpt4 = Divide(maxHFEMaxEventOnline14ORpt4, maxHFEMaxEventpt4, "ratio14ORpt4");
+
+    TH1D* minHFEMaxEventOnline16AND = new TH1D("minHFEMaxevent_online16AND", "HFEMax min with event and online 16 AND selection", 201, -0.5, 200.5);
+    TH1D* minHFEMaxEventOnline16ANDpt2 = new TH1D("minHFEMaxevent_online16AND_pt2", "HFEMax min with event and online 16 AND pt > 2 selection", 201, -0.5, 200.5);
+    TH1D* minHFEMaxEventOnline16ANDpt3 = new TH1D("minHFEMaxevent_online16AND_pt3", "HFEMax min with event and online 16 AND pt > 3 selection", 201, -0.5, 200.5);
+    TH1D* minHFEMaxEventOnline16ANDpt4 = new TH1D("minHFEMaxevent_online16AND_pt4", "HFEMax min with event and online 16 AND pt > 4 selection", 201, -0.5, 200.5);
+
+    HFEMax_Minimum(T, event_online16AND, minHFEMaxEventOnline16AND);
+    HFEMax_Minimum(T, event_online16AND_pt2, minHFEMaxEventOnline16ANDpt2);
+    HFEMax_Minimum(T, event_online16AND_pt3, minHFEMaxEventOnline16ANDpt3);
+    HFEMax_Minimum(T, event_online16AND_pt4, minHFEMaxEventOnline16ANDpt4);
+
+    TH1D* ratio16AND = Divide(minHFEMaxEventOnline16AND, minHFEMaxEvent, "ratio16AND");
+    TH1D* ratio16ANDpt2 = Divide(minHFEMaxEventOnline16ANDpt2, minHFEMaxEventpt2, "ratio16ANDpt2");
+    TH1D* ratio16ANDpt3 = Divide(minHFEMaxEventOnline16ANDpt3, minHFEMaxEventpt3, "ratio16ANDpt3");
+    TH1D* ratio16ANDpt4 = Divide(minHFEMaxEventOnline16ANDpt4, minHFEMaxEventpt4, "ratio16ANDpt4");
+
+    TH1D* maxHFEMaxEventOnline16OR = new TH1D("maxHFEMaxevent_online16OR", "HFEMax max with event and online 16 OR selection", 201, -0.5, 200.5);
+    TH1D* maxHFEMaxEventOnline16ORpt2 = new TH1D("maxHFEMaxevent_online16OR_pt2", "HFEMax max with event and online 16 OR pt > 2 selection", 201, -0.5, 200.5);
+    TH1D* maxHFEMaxEventOnline16ORpt3 = new TH1D("maxHFEMaxevent_online16OR_pt3", "HFEMax max with event and online 16 OR pt > 3 selection", 201, -0.5, 200.5);
+    TH1D* maxHFEMaxEventOnline16ORpt4 = new TH1D("maxHFEMaxevent_online16OR_pt4", "HFEMax max with event and online 16 OR pt > 4 selection", 201, -0.5, 200.5); 
+
+    HFEMax_Maximum(T, event_online16OR, maxHFEMaxEventOnline16OR);
+    HFEMax_Maximum(T, event_online16OR_pt2, maxHFEMaxEventOnline16ORpt2);
+    HFEMax_Maximum(T, event_online16OR_pt3, maxHFEMaxEventOnline16ORpt3);
+    HFEMax_Maximum(T, event_online16OR_pt4, maxHFEMaxEventOnline16ORpt4);
+
+    TH1D* ratio16OR = Divide(maxHFEMaxEventOnline16OR, maxHFEMaxEvent, "ratio16OR");
+    TH1D* ratio16ORpt2 = Divide(maxHFEMaxEventOnline16ORpt2, maxHFEMaxEventpt2, "ratio16ORpt2");
+    TH1D* ratio16ORpt3 = Divide(maxHFEMaxEventOnline16ORpt3, maxHFEMaxEventpt3, "ratio16ORpt3");
+    TH1D* ratio16ORpt4 = Divide(maxHFEMaxEventOnline16ORpt4, maxHFEMaxEventpt4, "ratio16ORpt4");
 
     /////////////////////////////
     /////////////////////////////
@@ -170,12 +251,50 @@ void histmaker(){
     /////////////////////////////
     /////////////////////////////
 
-    TFile* outfile = new TFile("online14AND.root", "RECREATE");
+    TFile* outfile = new TFile("online.root", "RECREATE");
     minHFEMaxEvent->Write();
+    minHFEMaxEventpt2->Write();
+    minHFEMaxEventpt3->Write();
+    minHFEMaxEventpt4->Write();
     minHFEMaxEventOnline14AND->Write();
+    minHFEMaxEventOnline14ANDpt2->Write();
+    minHFEMaxEventOnline14ANDpt3->Write();
+    minHFEMaxEventOnline14ANDpt4->Write();
     ratio14AND->Write();
-    outfile->Close();
-    
+    ratio14ANDpt2->Write();
+    ratio14ANDpt3->Write();
+    ratio14ANDpt4->Write();
 
+    maxHFEMaxEvent->Write();
+    maxHFEMaxEventpt2->Write();
+    maxHFEMaxEventpt3->Write();
+    maxHFEMaxEventpt4->Write();
+    maxHFEMaxEventOnline14OR->Write();
+    maxHFEMaxEventOnline14ORpt2->Write();
+    maxHFEMaxEventOnline14ORpt3->Write();
+    maxHFEMaxEventOnline14ORpt4->Write();
+    ratio14OR->Write();
+    ratio14ORpt2->Write();
+    ratio14ORpt3->Write();
+    ratio14ORpt4->Write();
+
+    minHFEMaxEventOnline16AND->Write();
+    minHFEMaxEventOnline16ANDpt2->Write();
+    minHFEMaxEventOnline16ANDpt3->Write();
+    minHFEMaxEventOnline16ANDpt4->Write();
+    ratio16AND->Write();
+    ratio16ANDpt2->Write();
+    ratio16ANDpt3->Write();
+    ratio16ANDpt4->Write();
+
+    maxHFEMaxEventOnline16OR->Write();
+    maxHFEMaxEventOnline16ORpt2->Write();
+    maxHFEMaxEventOnline16ORpt3->Write();
+    maxHFEMaxEventOnline16ORpt4->Write();
+    ratio16OR->Write(); 
+    ratio16ORpt2->Write();
+    ratio16ORpt3->Write();
+    ratio16ORpt4->Write();
+    outfile->Close();
 }
 
--- plotter.C
+++ plotter.C
@@ -50,20 +50,172 @@ void Plotter1D(vector<TObject*> objects,  // these vectors should all be the sam
 
 }
 
+
+//function to draw percent lines on ratio plot
+void DrawPercents(TF1 *func, double percent, float range_low, float range_high, Color_t color, TLegend *legend) 
+{
+    TF1 *f_diff0 = new TF1("f_diff0", Form("func(x) - %f", percent), range_low, range_high);
+    double x_cross = f_diff0->GetX(0, range_low, range_high);
+
+    TLine *vline = new TLine(x_cross, 0, x_cross, percent);
+    vline->SetLineColor(color);
+    vline->SetLineStyle(2);
+    vline->Draw("SAME");
+
+    TLine *hline = new TLine(0, percent, x_cross, percent);
+    hline->SetLineColor(color);
+    hline->SetLineStyle(2);
+    hline->Draw("SAME");
+
+    legend->AddEntry(vline, Form("%.0f%% at x = %.2f", percent*100, x_cross), "l");
+}
+
+void RatioPlot(TH1D *h,
+               const char* title,
+               const char* xAxisTitle ,
+               const char* yAxisTitle, 
+               const char* outputFileName = "plotter_output.pdf", 
+               int ptCut = 0,
+               //vector<float> init_guess,,
+               float xMin = 0.0,
+               float xMax = 200.0,
+               float yMin = 0.0,
+               float yMax = 1.01,
+               float legendX = 0.65,
+               float legendY = 0.15,
+               float fit_low = 5,
+               float fit_high = 200.0,                
+               vector<int> colors = {kBlack, kRed, kBlue, kGreen+2, kRed},
+               vector<float> percents = {0.9, 0.95, 0.99}) {
+
+
+    TCanvas *c = new TCanvas("c", "Canvas", 800, 600);
+    TLegend* legend = new TLegend(legendX, legendY, legendX + 0.25, legendY + 0.25);
+    legend->SetTextSize(0.03);
+    int colorIdx = 0;
+    int labelIdx = 0;
+    h->SetLineColor(colors[colorIdx]);
+    h->SetTitle(title);
+    h->GetXaxis()->SetTitle(xAxisTitle);
+    h->GetYaxis()->SetTitle(yAxisTitle);
+    h->GetXaxis()->SetRangeUser(xMin, xMax);
+    h->GetYaxis()->SetRangeUser(yMin, yMax);
+    h->SetMarkerStyle(20);
+    h->SetMarkerSize(1);
+    h->Draw("P");
+    legend->AddEntry(h, "HIJING OO", "p");
+    colorIdx++;
+    labelIdx++;
+    
+    TF1 *func = new TF1("func", "[0]*exp([1]*x) + [2]*exp([3]*x) + [4]", fit_low, fit_high);
+    func->SetParameters(-9.98, -0.498, 73.32, -1.30, 1.00);
+    //func->SetParameters(-0.5, -0.0633, -4.64, -0.469, 0.99);
+    h->Fit("func", "0", "", fit_low, fit_high);
+
+    func->SetLineColor(colors[colorIdx]);
+    func->SetLineWidth(2);
+    func->SetLineStyle(1);
+    func->Draw("SAME");
+    legend->AddEntry(func, "Fit", "l");
+    colorIdx++;
+    labelIdx++;
+
+    DrawPercents(func, percents[0], fit_low, fit_high, colors[colorIdx], legend);
+    DrawPercents(func, percents[1], fit_low, fit_high, colors[colorIdx + 1], legend);
+    DrawPercents(func, percents[2], fit_low, fit_high, colors[colorIdx + 2], legend);
+    
+    legend->Draw();
+
+    TLatex *tex = new TLatex();
+    tex->SetTextSize(0.03);
+    tex->SetNDC();
+    tex->DrawLatex(0.65, 0.85, "PVFilter");
+    tex->DrawLatex(0.65, 0.80, "ClusterCompatibilityFilter");
+    tex->DrawLatex(0.65, 0.75, "|VZ| < 15");
+    if (ptCut != 0)
+        tex->DrawLatex(0.15, 0.65, Form("p_{T} > %i GeV/c", ptCut));
+
+    c->SaveAs(outputFileName);
+
+}
+
 void plotter(){
     cout << "Start" << endl;
-    TFile* f = new TFile("online14AND.root");
-    Plotter1D(
-        {f->Get("ratio14AND")},
-        {kRed},
-        {"Ratio 14 AND"},
-        "minimum HFEMax [GeV]",
-        "maximum HFEMax [GeV]",
-        0.0, 200.0, 
-        0.0, 1.001,
-        0.7, 0.7,
-        0,
-        "online14AND.pdf"
-  );
+    gStyle->SetOptStat(0);
+    TFile* f = new TFile("online.root");
+
+    RatioPlot((TH1D*)f->Get("ratio14AND"), "Ratio of events with and without online 14 AND cut",
+    "minimum HFEMax [GeV]", "Ratio", "ratio14AND.pdf");
+    RatioPlot((TH1D*)f->Get("ratio14ANDpt2"),"Ratio of events with and without online 14 AND, pt > 2 cut",
+    "minimum HFEMax [GeV]", "Ratio", "ratio14ANDpt2.pdf", 2);
+    RatioPlot((TH1D*)f->Get("ratio14ANDpt3"),"Ratio of events with and without online 14 AND, pt > 3 cut",
+    "minimum HFEMax [GeV]", "Ratio", "ratio14ANDpt3.pdf", 3);
+    RatioPlot((TH1D*)f->Get("ratio14ANDpt4"),"Ratio of events with and without online 14 AND, pt > 4 cut",
+    "minimum HFEMax [GeV]", "Ratio", "ratio14ANDpt4.pdf", 4);
+
+    RatioPlot((TH1D*)f->Get("ratio14OR"),"Ratio of events with and without online 14 OR cut",
+    "maximum HFEMax [GeV]", "Ratio", "ratio14OR.pdf");
+    RatioPlot((TH1D*)f->Get("ratio14ORpt2"),"Ratio of events with and without online 14 OR, pt > 2 cut",
+    "maximum HFEMax [GeV]", "Ratio", "ratio14ORpt2.pdf", 2);
+    RatioPlot((TH1D*)f->Get("ratio14ORpt3"),"Ratio of events with and without online 14 OR, pt > 3 cut",
+    "maximum HFEMax [GeV]", "Ratio", "ratio14ORpt3.pdf", 3);
+    RatioPlot((TH1D*)f->Get("ratio14ORpt4"),"Ratio of events with and without online 14 OR, pt > 4 cut",
+    "maximum HFEMax [GeV]", "Ratio", "ratio14ORpt4.pdf", 4);
+
+    RatioPlot((TH1D*)f->Get("ratio16AND"),"Ratio of events with and without online 16 AND cut",
+    "minimum HFEMax [GeV]", "Ratio", "ratio16AND.pdf");
+    RatioPlot((TH1D*)f->Get("ratio16ANDpt2"),"Ratio of events with and without online 16 AND, pt > 2 cut",
+    "minimum HFEMax [GeV]", "Ratio", "ratio16ANDpt2.pdf", 2);
+    RatioPlot((TH1D*)f->Get("ratio16ANDpt3"),"Ratio of events with and without online 16 AND, pt > 3 cut",
+    "minimum HFEMax [GeV]", "Ratio", "ratio16ANDpt3.pdf", 3);
+    RatioPlot((TH1D*)f->Get("ratio16ANDpt4"),"Ratio of events with and without online 16 AND, pt > 4 cut",
+    "minimum HFEMax [GeV]", "Ratio", "ratio16ANDpt4.pdf", 4);
+
+    RatioPlot((TH1D*)f->Get("ratio16OR"),"Ratio of events with and without online 16 OR cut",
+    "maximum HFEMax [GeV]", "Ratio", "ratio16OR.pdf");
+    RatioPlot((TH1D*)f->Get("ratio16ORpt2"),"Ratio of events with and without online 16 OR, pt > 2 cut",
+    "maximum HFEMax [GeV]", "Ratio", "ratio16ORpt2.pdf", 2);
+    RatioPlot((TH1D*)f->Get("ratio16ORpt3"),"Ratio of events with and without online 16 OR, pt > 3 cut",
+    "maximum HFEMax [GeV]", "Ratio", "ratio16ORpt3.pdf", 3);
+    RatioPlot((TH1D*)f->Get("ratio16ORpt4"),"Ratio of events with and without online 16 OR, pt > 4 cut",
+    "maximum HFEMax [GeV]", "Ratio", "ratio16ORpt4.pdf", 4);
+    
+    /*RatioPlot((TH1D*)f->Get("ratio14OR"),"Ratio of events with and without online 14 OR cut",
+    "maximum HFEMax [GeV]",-0.5, 200.5, 0.0, 1.01,0.65, 0.15,
+    4.0, 200.0,"ratio14OR.pdf");
+    RatioPlot((TH1D*)f->Get("ratio14ORpt2"),"Ratio of events with and without online 14 OR pt > 2 cut",
+    "maximum HFEMax [GeV]",-0.5, 200.5, 0.0, 1.01,0.65, 0.15,
+    4.0, 200.0,"ratio14ORpt2.pdf", 2);
+    RatioPlot((TH1D*)f->Get("ratio14ORpt3"),"Ratio of events with and without online 14 OR pt > 3 cut",
+    "maximum HFEMax [GeV]",-0.5, 200.5, 0.0, 1.01,0.65, 0.15,
+    4.0, 200.0,"ratio14ORpt3.pdf", 3);
+    RatioPlot((TH1D*)f->Get("ratio14ORpt4"),"Ratio of events with and without online 14 OR pt > 4 cut",
+    "maximum HFEMax [GeV]",-0.5, 200.5, 0.0, 1.01,0.65, 0.15,
+    4.0, 200.0,"ratio14ORpt4.pdf", 4); 
+
+    RatioPlot((TH1D*)f->Get("ratio16AND"),"Ratio of events with and without online 16 AND cut",
+    "minimum HFEMax [GeV]",-0.5, 200.5, 0.0, 1.01,0.65, 0.15,4.0, 200.0,"ratio16AND.pdf");
+    RatioPlot((TH1D*)f->Get("ratio16ANDpt2"),"Ratio of events with and without online 16 AND pt > 2 cut",
+    "minimum HFEMax [GeV]",-0.5, 200.5, 0.0, 1.01,0.65, 0.15,
+    4.0, 200.0,"ratio16ANDpt2.pdf", 2);
+    RatioPlot((TH1D*)f->Get("ratio16ANDpt3"),"Ratio of events with and without online 16 AND pt > 3 cut",
+    "minimum HFEMax [GeV]",-0.5, 200.5, 0.0, 1.01,0.65, 0.15,
+    4.0, 200.0,"ratio16ANDpt3.pdf", 3);
+    RatioPlot((TH1D*)f->Get("ratio16ANDpt4"),"Ratio of events with and without online 16 AND pt > 4 cut",
+    "minimum HFEMax [GeV]",-0.5, 200.5, 0.0, 1.01,0.65, 0.15,
+    4.0, 200.0,"ratio16ANDpt4.pdf", 4);
+
+    RatioPlot((TH1D*)f->Get("ratio16OR"),"Ratio of events with and without online 16 OR cut",
+    "maximum HFEMax [GeV]",-0.5, 200.5, 0.0, 1.01,0.65, 0.15,
+    4.0, 200.0,"ratio16OR.pdf");
+    RatioPlot((TH1D*)f->Get("ratio16ORpt2"),"Ratio of events with and without online 16 OR pt > 2 cut",
+    "maximum HFEMax [GeV]",-0.5, 200.5, 0.0, 1.01,0.65, 0.15,
+    4.0, 200.0,"ratio16ORpt2.pdf", 2);
+    RatioPlot((TH1D*)f->Get("ratio16ORpt3"),"Ratio of events with and without online 16 OR pt > 3 cut",
+    "maximum HFEMax [GeV]",-0.5, 200.5, 0.0, 1.01,0.65, 0.15,
+    4.0, 200.0,"ratio16ORpt3.pdf", 3);
+    RatioPlot((TH1D*)f->Get("ratio16ORpt4"),"Ratio of events with and without online 16 OR pt > 4 cut",
+    "maximum HFEMax [GeV]",-0.5, 200.5, 0.0, 1.01,0.65, 0.15,
+    4.0, 200.0,"ratio16ORpt4.pdf", 4);*/
   cout << "Done!" << endl;
 }
