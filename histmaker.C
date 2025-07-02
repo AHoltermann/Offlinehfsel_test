9c6359b19e2d60d955afbca60ed680d250e90279
Abraham Holtermann <85201358+AHoltermann@users.noreply.github.com>
2025-07-02 17:59:21 +0200

    Create histmaker.C

---
+++ histmaker.C
@@ -0,0 +1,154 @@
+/// begin aslkdfj;aslkjf;lkj;lkjelja;shgasufandskm
+using namespace std;
+
+void Centrality(TTree* T, TCut t, TH1D* h){
+     T->Project(h->GetName(), "HiBin", t);
+}
+
+void HFEMaxPlus(TTree * T, TCut t, TH1D* h){
+     T->Project(h->GetName(), "HFEMaxPlus", t);
+}
+
+void HFEMaxMinus(TTree * T, TCut t, TH1D* h){
+     T->Project(h->GetName(), "HFEMaxMinus", t);
+}
+
+void HFEMax_Maximum(TTree* T, TCut t, TH1D* h) {
+    TString expr = Form("TMath::Max(%s,%s)", "HFEMaxPlus", "HFEMaxMinus");
+    T->Project(h->GetName(), expr, t);
+}
+
+void HFEMax_Minimum(TTree* T, TCut t, TH1D* h) {
+    TString expr = Form("TMath::Min(%s,%s)", "HFEMaxPlus", "HFEMaxMinus");
+    T->Project(h->GetName(), expr, t);
+}
+
+void HFEMaxMinScatter(TTree* T, TCut t, TH2D* h) {
+    TString exprX = "HFEMaxPlus";
+    TString exprY = "HFEMaxMinus";
+    T->Project(h->GetName(), exprX + ":" + exprY, t);
+}
+
+void HFEPlusMinusScatter(TTree* T, TCut t, TH2D* h) {
+    TString exprX = "HFEMaxPlus";
+    TString exprY = "HFEMaxMinus";
+    T->Project(h->GetName(), exprX + ":" + exprY, t);
+}
+
+Long64_t HowManyPass(TTree* T, TCut t){
+    Long64_t nPass = T->GetEntries(t);
+    return nPass;
+}
+
+TCut HFONLINE(int threshold, string andor){
+    
+    string expr = 
+        "mMaxL1HFAdcPlus > " +
+        std::to_string(threshold) + 
+        " " +
+        andor +  
+        " " +
+        "mMaxL1HFAdcMinus > " + 
+        std::to_string(threshold);
+
+    const char* cutExpr = expr.c_str();
+    TCut ezcut(cutExpr);
+
+    return ezcut;
+}
+
+TCut HFOFFLINE(int threshold, string andor){
+
+    string expr = 
+        "HFEMaxPlus > " + 
+        std::to_string(threshold) + 
+        " " + 
+        andor + 
+        " " +  
+        "HFEMaxMinus > " + 
+        std::to_string(threshold);
+
+    const char* cutExpr = expr.c_str();
+    TCut ezcut(cutExpr);
+
+    return ezcut;
+}
+
+TCut TRKPTCUT(float threshold){
+    string expr = "leadingPtEta1p0_sel > " + std::to_string(threshold);
+    const char* cutExpr = expr.c_str();
+    TCut ezcut(cutExpr);
+    return ezcut;
+}
+
+TH1D* Divide(TH1D*h1, TH1D*h2){
+    TH1D* h3 = (TH1D*)h1->Clone();
+    h3->Divide(h2);
+    return h3;
+}
+
+void histmaker(){
+
+    /////////////////////////////
+    /////////////////////////////
+    ////                     ////
+    ////  INPUT FILES HERE   ////
+    ////                     ////
+    /////////////////////////////
+    /////////////////////////////
+
+    TFile* f = new Tfile("INSERT HERE");
+    TTree* T = (TTree*)f->Get("Tree"); 
+
+    /////////////////////////////
+    /////////////////////////////
+    ////                     ////
+    ////  Event Selection    ////
+    ////                     ////
+    /////////////////////////////
+    /////////////////////////////
+
+    TCut VZcut("VZ > -15 && VZ < 15");
+    TCut PVcut("PVFilter == 1");
+    TCut CCcut("ClusterCompatibilityFilter == 1");
+    TCut Nvtxcut("NVtx > 0");
+    // use HFONLINE and HFOFFLINE functions for a tcut
+    // use TRKPT for track pt cuts
+
+    // EXAMPLE EVENT SELECTIONs
+    TCut ESel_Example1 = VZcut && PVcut && CCcut && Nvtxcut && HFONLINE(14, "&&");
+    TCut ESel_Example2 = VZcut && PVcut && CCcut && Nvtxcut && HFOFFLINE(10, "||") && HFONLINE(14, "||");
+
+    /////////////////////////////
+    /////////////////////////////
+    ////                     ////
+    ////  Generate Hists     ////
+    ////                     ////
+    /////////////////////////////
+    /////////////////////////////
+
+    TH1D* example1 = new TH1D("example", "Example Histogram", 201, -0.5, 200.5);
+    TH1D* example2 = new TH1D("example2", "Example Histogram 2", 201, -0.5, 200.5);
+
+    Centrality(T, ESel_Example, example);
+    Centrality(T, ESel_Example2, example2);
+
+    TH1D* cratio = Divide(example1, example2);
+    
+
+    /////////////////////////////
+    /////////////////////////////
+    ////                     ////
+    ////  Save to output     ////
+    ////                     ////
+    /////////////////////////////
+    /////////////////////////////
+
+    TFile* outfile = new TFile("output.root", "RECREATE");
+    example1->Write();
+    example2->Write();
+    cratio->Write();
+    outfile->Close();
+    
+
+}
